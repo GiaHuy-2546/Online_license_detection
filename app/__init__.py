2c7d4edaa8faa29d93f76d5d8da2bac3a7689244
GiaHuy-2546 <12345huy123@gmail.com>
2024-11-07 18:13:53 +0700

    suanua

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,7 @@
-from flask import Flask, render_template, request, send_from_directory
+from flask import Flask, render_template, request
 import os
 from Yolo_prediction import detect_license_plate
 import cv2
-import numpy as np
-from io import BytesIO
-from werkzeug.datastructures import FileStorage
 # webserver gateway interface
 app = Flask(__name__)
 
